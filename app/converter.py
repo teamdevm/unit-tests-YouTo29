3e53e5e6f977628ce713d43bade20d1f6798f7e8
Lera Ushakova <125185029+YouTo29@users.noreply.github.com>
2023-06-30 17:02:39 +0300

    Update converter.py

--- app/converter.py
+++ app/converter.py
@@ -2,12 +2,10 @@ import requests
 
 class Converter:
     def get_exchange_rate(self, currency: str) -> float:
-        # Метод возвращает курс указанной валюты
-        # url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-        
-        return None 
+        url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+        response = requests.get(url)
+        data = response.json()
+        return float(data['bpi'][currency]['rate_float'])
 
     def convert_bitcoins(self, currency: str, coins: int) -> float:
-        # Метод возвращает сколько стоит в указанной валюте заданное количество биткоинов
-        
-        return None
+        return coins * self.get_exchange_rate(currency)
